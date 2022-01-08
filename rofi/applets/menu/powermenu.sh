b103b28e1e5352fe56c8115c2613a225f58d1cfd
Nguyễn Nhật Huỳnh <Huynhb1303667@gmail.com>
2022-01-08 16:30:00 +0700

    Add files via upload

---
+++ rofi/applets/menu/powermenu.sh
@@ -0,0 +1,78 @@
+#!/usr/bin/env bash
+
+wdir="$HOME/.config/leftwm/themes/current"
+dir="$wdir/rofi/applets/menu/configs"
+rofi_command="rofi -theme $dir/powermenu.rasi"
+
+uptime=$(uptime -p | sed -e 's/up //g')
+cpu=$($wdir/rofi/bin/usedcpu)
+memory=$($wdir/rofi/bin/usedram)
+
+# Options
+shutdown=""
+reboot=""
+#lock=""
+#suspend=""
+hibernate=""
+logout=""
+
+# Confirmation
+confirm_exit() {
+	rofi -dmenu\
+		-i\
+		-no-fixed-num-lines\
+		-p "Are You Sure? : "\
+		-theme $wdir/rofi/applets/styles/confirm.rasi
+}
+
+# Message
+msg() {
+	rofi -theme "$wdir/rofi/applets/styles/message.rasi" -e "Available Options  -  yes / y / no / n"
+}
+
+# Variable passed to rofi
+options="$shutdown\n$reboot\n$hibernate\n$logout"
+
+chosen="$(echo -e "$options" | $rofi_command -p "祥  $uptime  |     $cpu  |  ﬙  $memory " -dmenu -selected-row 2)"
+case $chosen in
+    $shutdown)
+		ans=$(confirm_exit &)
+		if [[ $ans == "yes" || $ans == "YES" || $ans == "y" || $ans == "Y" ]]; then
+			loginctl poweroff
+		elif [[ $ans == "no" || $ans == "NO" || $ans == "n" || $ans == "N" ]]; then
+			exit 0
+        else
+			msg
+        fi
+        ;;
+    $reboot)
+		ans=$(confirm_exit &)
+		if [[ $ans == "yes" || $ans == "YES" || $ans == "y" || $ans == "Y" ]]; then
+			loginctl reboot
+		elif [[ $ans == "no" || $ans == "NO" || $ans == "n" || $ans == "N" ]]; then
+			exit 0
+        else
+			msg
+        fi
+        ;;
+    $hibernate)
+		ans=$(confirm_exit &)
+		if [[ $ans == "yes" || $ans == "YES" || $ans == "y" || $ans == "Y" ]]; then
+			loginctl hibernate
+		elif [[ $ans == "no" || $ans == "NO" || $ans == "n" || $ans == "N" ]]; then
+			exit 0
+        else
+			msg
+        fi
+        ;;
+    $logout)
+		ans=$(confirm_exit &)
+		if [[ $ans == "yes" || $ans == "YES" || $ans == "y" || $ans == "Y" ]]; then
+			$wdir/rofi/bin/logout
+		elif [[ $ans == "no" || $ans == "NO" || $ans == "n" || $ans == "N" ]]; then
+			exit 0
+        else
+			msg
+        fi
+        ;;
+esac
